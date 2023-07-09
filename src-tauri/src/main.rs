217713cf52413f50345dab11184e060b45d79d46
Andreas Schaeffer <andreas@schaeffernet.de>
2023-07-09 15:35:47 +0200

    Start with dev tools

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,15 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
+
+use tauri::Manager;
 
 fn main() {
     // Initialize RuntimeManager
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            window.open_devtools();
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
