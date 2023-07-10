b6d02ce6bd11c9c22cb448d12509330f9f15e89c
Andreas Schaeffer <andreas@schaeffernet.de>
2023-07-10 22:53:44 +0200

    Generate metadata about the build

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
-  tauri_build::build()
+    build_data::set_RUSTC_VERSION();
+    build_data::set_BUILD_DATE();
+    build_data::set_GIT_BRANCH();
+    build_data::set_GIT_COMMIT();
+    build_data::set_SOURCE_TIMESTAMP();
+    build_data::no_debug_rebuilds();
+    tauri_build::build()
 }
